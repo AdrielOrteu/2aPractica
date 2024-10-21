bcfaea89169453c5d3b330b9d72985bf0ac0d3a9
Adriel <1750927@uab.cat>
2024-10-21 16:28:40 +0200

    segundo passo, falta testing

--- 2paso.c
+++ 2paso.c
@@ -1,1 +1,11 @@
-C-code
+
+void PrintRow( float mat[N][N], int row, int from, int numel ) {
+  for (i=from; i < (from + numel); i++){
+    prinf("%f", mat[row][i];)
+    if i < (from + numel - 1) {
+      printf(" ");
+    }else{
+      printf("\n");
+    }
+  }
+}
